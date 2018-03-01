eebac6aa6e1d640311c825048eecbce6d43a3ba3
Sebastien Binet <seb.binet@gmail.com>
2018-03-01 09:43:37 +0100

    all: update to buffalo-v0.11.0

--- actions/posts.go
+++ actions/posts.go
@@ -3,7 +3,7 @@ package actions
 import (
 	"github.com/go-saloon/saloon/models"
 	"github.com/gobuffalo/buffalo"
-	"github.com/markbates/pop"
+	"github.com/gobuffalo/pop"
 	"github.com/pkg/errors"
 )
 
