49aead1f1b24ee36174f54f6979e0e6133ad913b
Sebastien Binet <seb.binet@gmail.com>
2018-03-10 17:27:25 +0100

    grifts: improve db-admin tasks

--- grifts/create_admin.go
+++ grifts/create_admin.go
@@ -16,6 +16,19 @@ import (
 
 var _ = grift.Namespace("db", func() {
 
+	grift.Add("setup", func(c *grift.Context) error {
+		for _, name := range []string{
+			"db:setup:create-admin",
+			"db:setup:create-dummy-users",
+		} {
+			err := grift.Run(name, c)
+			if err != nil {
+				return errors.WithStack(err)
+			}
+		}
+		return nil
+	})
+
 	grift.Namespace("setup", func() {
 
 		grift.Desc("create-admin", "Create a default admin account")
@@ -40,6 +53,33 @@ var _ = grift.Namespace("db", func() {
 			})
 		})
 
+		grift.Desc("create-dummy-users", "Create dummy user accounts")
+		grift.Add("create-dummy-users", func(c *grift.Context) error {
+			return models.DB.Transaction(func(tx *pop.Connection) error {
+				for _, usr := range []*models.User{
+					{
+						Username: "toto",
+						Email:    "toto@example.com",
+					},
+					{
+						Username: "tata",
+						Email:    "tata@example.com",
+					},
+				} {
+					usr.Password = usr.Username
+					pwd, err := bcrypt.GenerateFromPassword([]byte(usr.Password), bcrypt.DefaultCost)
+					if err != nil {
+						return fmt.Errorf("could not generate password hash: %v", err)
+					}
+					usr.PasswordHash = string(pwd)
+					err = tx.Create(usr)
+					if err != nil {
+						return errors.WithStack(err)
+					}
+				}
+				return nil
+			})
+		})
 	})
 
 })
