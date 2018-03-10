59e094fbb45dad451814d44428475abf8341970e
Sebastien Binet <seb.binet@gmail.com>
2018-03-10 14:28:33 +0100

    all: add basic topic creation

--- actions/topics.go
+++ actions/topics.go
@@ -1,6 +1,11 @@
 package actions
 
-import "github.com/gobuffalo/buffalo"
+import (
+	"github.com/go-saloon/saloon/models"
+	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/pop"
+	"github.com/pkg/errors"
+)
 
 // TopicsIndex default implementation.
 func TopicsIndex(c buffalo.Context) error {
@@ -8,11 +13,44 @@ func TopicsIndex(c buffalo.Context) error {
 }
 
 func TopicsCreateGet(c buffalo.Context) error {
-	return c.Render(200, r.HTML("topics/create.html"))
+	tx := c.Value("tx").(*pop.Connection)
+	topic := &models.Topic{}
+	c.Set("topic", topic)
+	cat := &models.Category{}
+	if err := tx.Find(cat, c.Param("cid")); err != nil {
+		return c.Error(404, err)
+	}
+	c.Set("category", cat)
+	topic.CategoryID = cat.ID
+
+	return c.Render(200, r.HTML("topics/create"))
 }
 
 func TopicsCreatePost(c buffalo.Context) error {
-	return c.Render(200, r.HTML("topics/create.html"))
+	tx := c.Value("tx").(*pop.Connection)
+	topic := &models.Topic{}
+	user := c.Value("current_user").(*models.User)
+	if err := c.Bind(topic); err != nil {
+		return errors.WithStack(err)
+	}
+	cat := &models.Category{}
+	if err := tx.Find(cat, c.Param("cid")); err != nil {
+		return c.Error(404, err)
+	}
+	topic.AuthorID = user.ID
+	topic.CategoryID = cat.ID
+	// Validate the data from the html form
+	verrs, err := tx.ValidateAndCreate(topic)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	if verrs.HasAny() {
+		c.Set("topic", topic)
+		c.Set("errors", verrs.Errors)
+		return c.Render(422, r.HTML("topics/create"))
+	}
+	c.Flash().Add("success", "New topic added successfully.")
+	return c.Redirect(302, "/topics/detail/%s", topic.ID)
 }
 
 // TopicsEdit default implementation.
@@ -27,5 +65,14 @@ func TopicsDelete(c buffalo.Context) error {
 
 // TopicsDetail default implementation.
 func TopicsDetail(c buffalo.Context) error {
-	return c.Render(200, r.HTML("topics/detail.html"))
+	tx := c.Value("tx").(*pop.Connection)
+	topic := &models.Topic{}
+	if err := c.Bind(topic); err != nil {
+		return errors.WithStack(err)
+	}
+	if err := tx.Find(topic, c.Param("tid")); err != nil {
+		return c.Error(404, err)
+	}
+	c.Set("topic", topic)
+	return c.Render(200, r.HTML("topics/detail"))
 }
