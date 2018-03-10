71e43fba140919bd093451296b1c17f377e62c2b
Sebastien Binet <seb.binet@gmail.com>
2018-03-10 17:57:19 +0100

    grifts: cosmetics

--- grifts/create_admin.go
+++ grifts/create_admin.go
@@ -35,10 +35,6 @@ var _ = grift.Namespace("db", func() {
 		grift.Desc("create-admin", "Create a default admin account")
 		grift.Add("create-admin", func(c *grift.Context) error {
 			return models.DB.Transaction(func(tx *pop.Connection) error {
-				err := tx.TruncateAll()
-				if err != nil {
-					return errors.WithStack(err)
-				}
 				usr := &models.User{
 					Username: "admin",
 					Email:    "admin@example.com",
@@ -68,6 +64,7 @@ var _ = grift.Namespace("db", func() {
 					},
 				} {
 					usr.Password = usr.Username
+					usr.PasswordConfirm = usr.Username
 					pwd, err := bcrypt.GenerateFromPassword([]byte(usr.Password), bcrypt.DefaultCost)
 					if err != nil {
 						return fmt.Errorf("could not generate password hash: %v", err)
