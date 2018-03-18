8d222333606158e08f6411c027428aab4df5fd5a
Sebastien Binet <seb.binet@gmail.com>
2018-03-18 15:17:45 +0100

    mailers: add proper configuration of mail-notifications

--- mailers/mailers.go
+++ mailers/mailers.go
@@ -6,29 +6,58 @@ package mailers
 
 import (
 	"bytes"
+	"encoding/binary"
+	"encoding/gob"
+	"io"
 	"log"
+	"net"
 	"text/template"
 
 	"github.com/gobuffalo/buffalo/mail"
 	"github.com/gobuffalo/buffalo/render"
 	"github.com/gobuffalo/envy"
 	"github.com/gobuffalo/packr"
+	"github.com/pkg/errors"
 )
 
 var smtp mail.Sender
 var r *render.Engine
 
+var notify struct {
+	ReplyTo    string
+	MessageID  string
+	InReplyTo  string
+	ListID     string
+	SubjectHdr string
+	From       string
+}
+
 func init() {
 
 	// Pulling config from the env.
+	send := envy.Get("SALOON_SEND_MAIL", "")
 	port := envy.Get("SMTP_PORT", "1025")
 	host := envy.Get("SMTP_HOST", "localhost")
 	user := envy.Get("SMTP_USER", "")
 	password := envy.Get("SMTP_PASSWORD", "")
 
+	notify.ReplyTo = envy.Get("SALOON_MAIL_NOTIFY_REPLY_TO", "")
+	notify.MessageID = envy.Get("SALOON_MAIL_NOTIFY_MESSAGE_ID", "")
+	notify.InReplyTo = envy.Get("SALOON_MAIL_NOTIFY_IN_REPLY_TO", "")
+	notify.ListID = envy.Get("SALOON_MAIL_NOTIFY_LIST_ID", "")
+	notify.SubjectHdr = envy.Get("SALOON_MAIL_NOTIFY_SUBJECT_HDR", "")
+	notify.From = envy.Get("SALOON_MAIL_NOTIFY_FROM", "")
+
 	var err error
 
-	smtp, err = mail.NewSMTPSender(host, port, user, password)
+	switch send {
+	case "1", "y", "yes", "Y":
+		smtp, err = mail.NewSMTPSender(host, port, user, password)
+	case "remote":
+		smtp, err = newRelaySender(host, port, user, password)
+	default:
+		smtp = noMailSender{}
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -63,4 +92,57 @@ func PlainTextTemplateEngine(input string, data map[string]interface{}, helpers 
 	bb := &bytes.Buffer{}
 	err = t.Execute(bb, data)
 	return bb.String(), err
+}
+
+// noMailSender is a mail.Sender with no actual delivery.
+type noMailSender struct{}
+
+func (noMailSender) Send(mail.Message) error {
+	return nil
+}
+
+type relaySender struct {
+	addr string
+}
+
+func newRelaySender(host, port, user, password string) (*relaySender, error) {
+	return &relaySender{host + ":" + port}, nil
+}
+
+func (rs relaySender) Send(m mail.Message) error {
+	conn, err := net.Dial("tcp", rs.addr)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	defer conn.Close()
+
+	o := new(bytes.Buffer)
+	for i := range m.Attachments {
+		att := &m.Attachments[i]
+		r := new(bytes.Buffer)
+		_, err := io.Copy(r, att.Reader)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		att.Reader = r
+	}
+
+	err = gob.NewEncoder(o).Encode(m)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	var hdr [8]byte
+	n := int64(o.Len())
+	binary.BigEndian.PutUint64(hdr[:], uint64(n))
+	_, err = conn.Write(hdr[:])
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	_, err = conn.Write(o.Bytes())
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
 }
--- mailers/notify.go
+++ mailers/notify.go
@@ -13,37 +13,15 @@ import (
 	"github.com/pkg/errors"
 )
 
-/*
-Transport; Wed, 14 Mar 2018 17:30:38 +0000
-Received: from localhost.localdomain (188.184.69.125) by cernmx.cern.ch (188.184.36.24) with Microsoft SMTP Server id 14.3.319.2; Wed, 14 Mar 2018 18:30:21 +0100
-Return-Path: replies+verp-435445b6111626719fedf95e46d81dc9@root-forum-mail.cern.ch
-Date: Wed, 14 Mar 2018 17:30:20 +0000
-From: Viesturs <root.discourse@cern.ch>
-Reply-To: ROOT Forum <replies+ee5527776516bff82feb239da4e449ca@root-forum-mail.cern.ch>
-To: <seb.binet@gmail.com>
-Message-ID: <topic/28306/124700@root-forum.cern.ch>
-In-Reply-To: <topic/28306/124690@root-forum.cern.ch>
-References: <topic/28306@root-forum.cern.ch> <topic/28306/124690@root-forum.cern.ch>
-Subject: [ROOT Forum] [ROOT] Load a standalone sharedlibrary
-MIME-Version: 1.0
-Content-Type: multipart/alternative; boundary="--==_mimepart_5aa95c2c8115b_1ff3fd51e9899e05662fb"; charset="UTF-8"
-Content-Transfer-Encoding: 7bit
-List-Unsubscribe: <http://root-forum.cern.ch/email/unsubscribe/955b9d9ea7d609b54171b3b7e0ca582dd6dc3832c88860999c80c9e5eae5fb62>
-X-Auto-Response-Suppress: All
-Auto-Submitted: auto-generated
-Precedence: list
-List-ID: <root.root-forum.cern.ch>
-List-Archive: http://root-forum.cern.ch/t/load-a-standalone-sharedlibrary/28306
-*/
-
 func NewTopicNotify(c buffalo.Context, topic *models.Topic, recpts []models.User) error {
 	// Creates a new message
 	m := mail.NewMessage()
-	m.SetHeader("Message-ID", fmt.Sprintf("<topic/%s@saloon.com>", topic.ID))
-	m.SetHeader("List-ID", "forum.saloon.com")
+	m.SetHeader("Reply-To", notify.ReplyTo)
+	m.SetHeader("Message-ID", fmt.Sprintf("<topic/%s@%s>", topic.ID, notify.MessageID))
+	m.SetHeader("List-ID", notify.ListID)
 
-	m.From = fmt.Sprintf("%s <alternatiba63.forum@gmail.com>", topic.Author.Username)
-	m.Subject = topic.Title
+	m.Subject = notify.SubjectHdr + " " + topic.Title
+	m.From = fmt.Sprintf("%s <%s>", topic.Author.Username, notify.From)
 	m.To = nil
 	m.Bcc = nil
 	for _, usr := range recpts {
@@ -76,12 +54,13 @@ func NewTopicNotify(c buffalo.Context, topic *models.Topic, recpts []models.User
 func NewReplyNotify(c buffalo.Context, topic *models.Topic, reply *models.Reply, recpts []models.User) error {
 	// Creates a new message
 	m := mail.NewMessage()
-	m.SetHeader("Message-ID", fmt.Sprintf("<topic/%s/%s@saloon.com>", topic.ID, reply.ID))
-	m.SetHeader("In-Reply-To", fmt.Sprintf("<topic/%s@saloon.com>", topic.ID))
-	m.SetHeader("List-ID", "forum.saloon.com")
+	m.SetHeader("Reply-To", notify.ReplyTo)
+	m.SetHeader("Message-ID", fmt.Sprintf("<topic/%s/%s@%s>", topic.ID, reply.ID, notify.MessageID))
+	m.SetHeader("In-Reply-To", fmt.Sprintf("<topic/%s@%s>", topic.ID, notify.InReplyTo))
+	m.SetHeader("List-ID", notify.ListID)
 
-	m.From = fmt.Sprintf("%s <alternatiba63.forum@gmail.com>", reply.Author.Username)
-	m.Subject = topic.Title
+	m.Subject = notify.SubjectHdr + " " + topic.Title
+	m.From = fmt.Sprintf("%s <%s>", reply.Author.Username, notify.From)
 	m.To = nil
 	m.Bcc = nil
 	for _, usr := range recpts {
