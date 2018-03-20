24d93d65c34116664d7fbc5c7fbf0aa319b46a23
Sebastien Binet <seb.binet@gmail.com>
2018-03-20 14:21:12 +0100

    models: add users creation test

--- models/category_test.go
+++
@@ -1,7 +0,0 @@
-package models_test
-
-import "testing"
-
-func Test_Category(t *testing.T) {
-	t.Fatal("This test needs to be implemented!")
-}
--- models/user_test.go
+++ models/user_test.go
@@ -1,7 +1,52 @@
+// Copyright 2018 The go-saloon Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package models_test
 
-import "testing"
+import "github.com/go-saloon/saloon/models"
 
-func Test_User(t *testing.T) {
-	t.Fatal("This test needs to be implemented!")
+func (ms *ModelSuite) Test_User_Create() {
+	n, err := ms.DB.Count("users")
+	ms.NoError(err)
+	ms.Equal(0, n)
+
+	u := &models.User{
+		Username:        "usr1",
+		Email:           "user@example.com",
+		Password:        "password",
+		PasswordConfirm: "password",
+	}
+	ms.Zero(u.PasswordHash)
+
+	verrs, err := u.Create(ms.DB)
+	ms.NoError(err)
+	ms.False(verrs.HasAny())
+	ms.NotZero(u.PasswordHash)
+	ms.False(u.Admin)
+
+	n, err = ms.DB.Count("users")
+	ms.NoError(err)
+	ms.Equal(1, n)
+}
+
+func (ms *ModelSuite) Test_User_Create_ValidationErrors() {
+	n, err := ms.DB.Count("users")
+	ms.NoError(err)
+	ms.Equal(0, n)
+
+	u := &models.User{
+		Email:           "user@example.com",
+		Password:        "password",
+		PasswordConfirm: "password",
+	}
+	ms.Zero(u.PasswordHash)
+
+	verrs, err := u.Create(ms.DB)
+	ms.NoError(err)
+	ms.True(verrs.HasAny())
+
+	n, err = ms.DB.Count("users")
+	ms.NoError(err)
+	ms.Equal(0, n)
 }
